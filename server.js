7f09eb6116c03f819679331002d4e63250ccc534
ziping <garfield654123@gmail.com>
2021-05-27 20:23:06 +0800

    try to update to github

--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 
 const http = require("http");
 const sever = http.createServer((request, response) =>{
-response.end("hello form nodejs server");
+response.end("hello from nodejs server2");
 
 });
 
 const port =3000;  //assign the default port 
 const ip="127.0.0.1" // point to the local sever
 sever.listen(port, ip, () =>{
-    console.log('Sever is running at http://${ip}:${port}');
+    console.log(`Server is running at http://${ip}:${port}`);
 });
 /* sever.listen (1.port, 2IP, 3.callvack function)*/ 
  

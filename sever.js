629aa25bfe7fb576365d59558f6ea8b5f63992cc
ziping <garfield654123@gmail.com>
2021-05-27 14:33:16 +0800

    add the new server

--- sever.js
+++ sever.js
@@ -9,7 +9,7 @@ response.end("hello form nodejs server");
 const port =3000;  //assign the default port 
 const ip="127.0.0.1" // point to the local sever
 sever.listen(port, ip, () =>{
-    console.log('sever is running at http://${ip}:${port}');
+    console.log('Sever is running at http://${ip}:${port}');
 });
 /* sever.listen (1.port, 2IP, 3.callvack function)*/ 
  
